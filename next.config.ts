1eabf0280b7b93a41c54dfb3646300d11558cf03
kobadze888 <mr.kobadze@gmail.com>
2025-12-19 16:45:58 +0400

    Fix-redis

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,22 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
   images: {
+    // ✅ ჩართულია AVIF და WebP მხარდაჭერა სუპერ სისწრაფისთვის
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
-      { protocol: 'https', hostname: 'chantashop.ge' }, // შენი WP დომენი
+      { protocol: 'https', hostname: 'chantashop.ge' },
       { protocol: 'https', hostname: 'secure.gravatar.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
+  // ✅ პროდაქშენზე აშორებს ზედმეტ ლოგებს (Console log)
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  // ✅ აჩქარებს ლოდინის დროს მძიმე ბიბლიოთეკებისთვის
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
+  }
 };
 
 export default withNextIntl(nextConfig);
