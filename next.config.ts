e871a42dee280a99a0dcc099b6cf62e47d7395b0
kobadze888 <mr.kobadze@gmail.com>
2025-12-21 20:29:08 +0400

    fix-image-error3

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
   compress: true, 
   images: {
     // ოპტიმიზაცია ჩართულია!
-    formats: ['image/avif', 'image/webp'], 
+    formats: ['image/webp'], 
     minimumCacheTTL: 31536000, 
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
